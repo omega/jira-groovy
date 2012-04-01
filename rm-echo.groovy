94bd70c7a327bd0ac9bfb1efc4d48d3043912958
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-04-01 11:06:30 +0200

    Add action.name

--- rm-echo.groovy
+++ rm-echo.groovy
@@ -1,8 +1,9 @@
 import com.atlassian.jira.issue.Issue
 Issue issue = issue
+action = transientVars.get("descriptor").getAction(transientVars.get("actionId"))
 
 netcat = new Socket("noops1.startsiden.no", 54321)
 netcat.withStreams { input, output ->
-  output << "#drift ${issue.key} ${issue.summary} \n"
+  output << "#drift ${issue.key} ${issue.summary} ${action.name}\n"
   buffer = input.newReader().readLine()
 }
