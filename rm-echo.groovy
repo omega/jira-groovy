f71501c4a7f0930bc99fd035fda60f355258ab38
Andreas Marienborg <andreas.marienborg@gmail.com>
2012-04-01 11:11:21 +0200

    Add assignee.name

--- rm-echo.groovy
+++ rm-echo.groovy
@@ -4,6 +4,6 @@ action = transientVars.get("descriptor").getAction(transientVars.get("actionId")
 
 netcat = new Socket("noops1.startsiden.no", 54321)
 netcat.withStreams { input, output ->
-  output << "#drift ${issue.key} ${issue.summary} ${action.name}\n"
+  output << "#drift ${issue.key} ${issue.summary} ${action.name} ${issue.getAssigneeUser().getDisplayName()}\n"
   buffer = input.newReader().readLine()
 }
