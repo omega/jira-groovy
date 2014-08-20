eed7fefd6b671b60d71f98036d5f6ed6512825b2
Andreas Marienborg <omega@palle.net>
2014-08-20 10:13:52 +0700

    Loop-de-loop, cleanups

--- graphite.groovy
+++ graphite.groovy
@@ -6,13 +6,16 @@ import com.atlassian.jira.ComponentManager
 import com.atlassian.jira.issue.fields.CustomField
 
 log = Category.getInstance("no.startsiden.no.jira.groovy.graphite")
-def team_field = "customfield_10520" // XXX: This is not pretty to hardcode?
+// XXX: These thingd are hard-coded here, because I haven't found a good way to get
+// config from anywhere for script runner scripts
+def hosts = ["192.168.30.23", "192.168.30.113"]
+def team_field = "customfield_10520"
 stages = [
     41:"prod",
     21:"kua",
 ]
 
-long unixTime = System.currentTimeMillis() / 1000L;
+long unixTime = System.currentTimeMillis() / 1000L; // / # Vim fuckups!
 
 Issue issue = issue
 
@@ -27,40 +30,21 @@ CustomField customFieldSrc = customFieldManager.getCustomFieldObject(team_field)
 team = issue.getCustomFieldValue(customFieldSrc).toString().toLowerCase()
 stage = stages.get(action)
 
-log.error("team: ${team}, action: ${action} stage: ${stage}")
-try {
-    netcat = new Socket("192.168.30.23", 2003)
-    netcat.withStreams { input, output ->
-        msg = "events.deploy.${stage}.${team} 1 ${unixTime}\n"
-log.error("msg: ${msg}")
-        output << "${msg}"
-        netcat.close()
+log.error("team: ${team}, action: ${action} stage: ${stage} issue: ${issue.key}")
+for (host in hosts)
+    try {
+        netcat = new Socket(host, 2003)
+        netcat.withStreams { input, output ->
+            msg = "events.deploy.${stage}.${team} 1 ${unixTime}\n"
+            output << "${msg}"
+            netcat.close()
+        }
+        netcat = new Socket(host, 2003)
+        netcat.withStreams { input, output ->
+            msg = "events.deploy.${stage}.${team}.${issue.key} 1 ${unixTime}\n"
+            netcat.close()
+        }
+    } catch (ex) {
+        log.error("Exception doing netcat to ${host}: ${ex}");
     }
-    netcat = new Socket("192.168.30.23", 2003)
-    netcat.withStreams { input, output ->
-        msg = "events.deploy.${stage}.${team}.${issue.key} 1 ${unixTime}\n"
-//        output << "${msg}"
-        netcat.close()
-    }
-} catch (ex) {
-    log.error("Exception doing netcat to dev-rolf: ${ex}");
-}
-
-try {
-    netcat = new Socket("192.168.30.113", 2003)
-    netcat.withStreams { input, output ->
-        msg = "events.deploy.${stage}.${team} 1 ${unixTime}\n"
-log.error("msg: ${msg}")
-        output << "${msg}"
-        netcat.close()
-    }
-    netcat = new Socket("192.168.30.113", 2003)
-    netcat.withStreams { input, output ->
-        msg = "events.deploy.${stage}.${team}.${issue.key} 1 ${unixTime}\n"
-//        output << "${msg}"
-        netcat.close()
-    }
-} catch (ex) {
-    log.error("Exception doing netcat to rd03: ${ex}");
-}
 
